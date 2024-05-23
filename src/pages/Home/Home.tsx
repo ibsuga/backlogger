b6942835400fdebda07d8481f88daee38000e2a3
Suga <okamisuga@gmail.com>
2024-05-23 17:15:26 +0200

    Hotfix:  removed unused home imports

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,6 @@
 import './Home.css';
 import useGameStore from '../../stores/useGameStore';
-import CompletedGames from '../../Components/CompletedGames/CompletedGames';
 import GameCollection from '../../Components/GameCollection/GameCollection';
-import NowPlayingGames from '../../Components/NowPlayingGames/NowPlayingGames';
 import GameReleasesList from '../../Components/GameReleasesList/GameReleasesList';
 import ActivitiesWidget from '../../Components/ActivitiesWidget/ActivitiesWidget';
 
